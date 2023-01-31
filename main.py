1c0d06e1b4cc58f6a0eab684223816d867043bdf
navronaman <namanmaulikshah@gmail.com>
2023-01-31 18:43:23 -0500

    Setup basic camera

--- main.py
+++ main.py
@@ -5,11 +5,24 @@ import cv2
 import time
 
 video = cv2.VideoCapture(0)
-time.sleep(1)
+time.sleep(3)
+
+first_frame = None
 
 while True:
     check, frame = video.read()
-    cv2.imshow("My video", frame)
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray_frame_gau = cv2.GaussianBlur(gray_frame, (21, 21), 0)
+
+    if first_frame is None:
+        first_frame = gray_frame_gau
+
+    delta_frame = cv2.absdiff(first_frame, gray_frame_gau)
+    cv2.imshow("My video", delta_frame)
+
+    cv2.threshold(delta_frame, 50, 255, cv2.THRESH_BINARY)
+
+
 
     key = cv2.waitKey(1)
 
