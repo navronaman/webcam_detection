01987405d24acff54ce23c8c692c62b3b6c608fb
navronaman <namanmaulikshah@gmail.com>
2023-01-31 19:55:17 -0500

    Created send_email function

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from emailing import send_email
+
 import cv2
 import time
 
@@ -8,8 +10,10 @@ video = cv2.VideoCapture(0)
 time.sleep(1)
 
 first_frame = None
+status_list = []
 
 while True:
+    status = 0
     check, frame = video.read()
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray_frame_gau = cv2.GaussianBlur(gray_frame, (21, 21), 0)
@@ -29,7 +33,17 @@ while True:
         if cv2.contourArea(contour) < 15000:
             continue
         x, y, w, h = cv2.boundingRect(contour)
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0))
+        rectangle = cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0))
+
+        if rectangle.any():
+            status = 1
+
+    status_list.append(status)
+    status_list = status_list[-2:]
+    print(status_list)
+
+    if status_list[0] == 1 and status_list[1] == 0:
+        send_email()
 
     cv2.imshow("My video", frame)
 
